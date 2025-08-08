10c44e24d81b81e5dd463b649194fa3f0220edbd
djakovm <martindjakov03@gmail.com>
2025-08-08 14:09:45 +0200

    Add paging to Repository

--- backend/src/main/kotlin/com/sorsix/backend/repository/CaseRepository.kt
+++ backend/src/main/kotlin/com/sorsix/backend/repository/CaseRepository.kt
@@ -1,33 +1,22 @@
 package com.sorsix.backend.repository
 
 import com.sorsix.backend.domain.Case
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 
 interface CaseRepository : JpaRepository<Case, Long> {
-    fun findAllByPublicTrue(): List<Case>
-    fun findAllByPublicFalse(): List<Case>
-    fun findAllByPatientId(patientId: Long): List<Case>
 
-    @Query(
-        """
-    SELECT c FROM Case c 
-    LEFT JOIN FETCH c.patient 
-    LEFT JOIN FETCH c.doctor 
-    LEFT JOIN FETCH c.examinations e
-    LEFT JOIN FETCH e.doctor
-    """
-    )
-    fun findAllWithDetails(): List<Case>
+    @EntityGraph(attributePaths = ["patient", "doctor"])
+    override fun findAll(pageable: Pageable): Page<Case>
 
-    @Query("""
-        SELECT c FROM Case c 
-        LEFT JOIN FETCH c.patient 
-        LEFT JOIN FETCH c.doctor 
-        WHERE c.id = :id
-    """)
-    fun findByIdWithDetails(@Param("id") id: Long): Case?
+    @EntityGraph(attributePaths = ["patient", "doctor"])
+    fun findAllByPublicTrue(pageable: Pageable): Page<Case>
 
+    @EntityGraph(attributePaths = ["patient", "doctor"])
+    fun findAllByPublicFalse(pageable: Pageable): Page<Case>
 
+    @EntityGraph(attributePaths = ["patient", "doctor"])
+    fun findAllByPatientId(patientId: Long, pageable: Pageable): Page<Case>
 }
